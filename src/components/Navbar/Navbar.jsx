de87c1b5ba034660d0d94a8fcf00adc74579265f
minhaj.dev <minhajhasan.dev@gmail.com>
2024-09-19 23:23:21 +0600

    added conditional nav render and navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,43 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-
+  return (
+    <div className="bg-[#1E3799] w-full ">
+      <div className="navbar bg-[#1E3799] max-w-screen-xl  border-none shadow-none mx-auto rounded-sm">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item font-bold text-white text-xl">
+            MeetUp
+          </Link>
         </div>
-    );
+        <div className="navbar-end">
+          <div className="avatar avatar-ring avatar-md">
+            <div className="dropdown-container">
+              <div className="dropdown">
+                <label
+                  className="btn btn-ghost flex cursor-pointer px-0"
+                  tabIndex="0"
+                >
+                  <img
+                    src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+                    alt="avatar"
+                  />
+                </label>
+                <div className="dropdown-menu dropdown-menu-bottom-left">
+                  <a className="dropdown-item text-sm">Profile</a>
+                  <a tabIndex="-1" className="dropdown-item text-sm">
+                    Account settings
+                  </a>
+                  <a tabIndex="-1" className="dropdown-item text-sm">
+                    Subscriptions
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,11 @@ import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   return (
-    <div className="relative h-full w-full">
+    <div className="relative h-full w-full max-w-screen-xl mx-auto">
       <Helmet>
         <title>Home - MeetUp</title>
       </Helmet>
-      <div>
+      <div className="">
         <p>Today we will work in this page</p>
       </div>
     </div>
