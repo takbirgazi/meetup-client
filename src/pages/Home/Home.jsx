af6da37c67b34f5f5a72f29ebd081f8279d019ee
minhaj.dev <minhajhasan.dev@gmail.com>
2024-09-19 22:15:31 +0600

    initiated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,15 @@
 import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-    return (
-        <div className="relative h-full w-full">
-            <Helmet>
-                <title>Home - MeetUp</title>
-            </Helmet>
-            <div>
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative h-full w-full">
+      <Helmet>
+        <title>Home - MeetUp</title>
+        <p>Today we will work in this page</p>
+      </Helmet>
+      <div></div>
+    </div>
+  );
 };
 
 export default Home;
