c4b265b501869e1fe1d95619847ecddd6f28d7aa
GaziMaksudur <gazimaksudur2@gmail.com>
2024-09-20 01:02:36 +0600

    firebase secrets are open now

--- src/providers/FirebaseProivder/FirebaseProvider.jsx
+++ src/providers/FirebaseProivder/FirebaseProvider.jsx
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: 'AIzaSyBmEm_NrtiLJkF8TqDlVZQ3nUVgUsvsh8o',
+  authDomain: 'tech-thunders-meet.firebaseapp.com',
+  projectId: 'tech-thunders-meet',
+  storageBucket: 'tech-thunders-meet.appspot.com',
+  messagingSenderId: '845122047501',
+  appId: '1:845122047501:web:b2e1601d6d8d525ddc4f44',
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
