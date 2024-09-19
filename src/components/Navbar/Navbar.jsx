bbf4ce7b48b6dd3d16f688b5d62bc926fd3740a0
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-09-20 00:17:04 +0600

    added: navbar for logged user

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import logo1 from "../../assets/logo1.png";
+import moment from 'moment';
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdLogout } from "react-icons/md";
+import { IoMdPerson } from "react-icons/io";
+
 
 const Navbar = () => {
   return (
@@ -6,10 +12,18 @@ const Navbar = () => {
       <div className="navbar bg-[#1E3799] max-w-screen-xl  border-none shadow-none mx-auto rounded-sm">
         <div className="navbar-start">
           <Link to="/" className="navbar-item font-bold text-white text-xl">
-            MeetUp
+            <img src={logo1} className="h-[42px] w-[100px]"/>
           </Link>
         </div>
         <div className="navbar-end">
+          {/* Current Time */}
+        <div className="text-white font-semibold text-[18px]">
+          {moment().format('h:mm A - ')}
+          {moment().format('ll')}
+        </div>
+
+        <IoSettingsOutline className="mx-4 text-white text-2xl" />
+
           <div className="avatar avatar-ring avatar-md">
             <div className="dropdown-container">
               <div className="dropdown">
@@ -19,17 +33,16 @@ const Navbar = () => {
                 >
                   <img
                     src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                    alt="avatar"
+                    alt="avatar" 
                   />
                 </label>
                 <div className="dropdown-menu dropdown-menu-bottom-left">
-                  <a className="dropdown-item text-sm">Profile</a>
-                  <a tabIndex="-1" className="dropdown-item text-sm">
-                    Account settings
-                  </a>
-                  <a tabIndex="-1" className="dropdown-item text-sm">
-                    Subscriptions
-                  </a>
+                  <p className="dropdown-item text-sm flex-row items-center gap-2">Hafsa <IoMdPerson /></p>
+                  
+                  <NavLink tabIndex="-1" className="dropdown-item text-sm flex-row items-center gap-2">
+                    Logout <MdLogout />
+                  </NavLink>
+
                 </div>
               </div>
             </div>
