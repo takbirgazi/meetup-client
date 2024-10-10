1627acf280cb5ca6625414cae48a10dc53befc72
GaziMaksudur <gazimaksudur2@gmail.com>
2024-10-10 16:27:27 +0600

    basic nav functionality initiated

--- src/pages/Landing/LandingComponents/NavComponent/NavBar.jsx
+++ src/pages/Landing/LandingComponents/NavComponent/NavBar.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../../../assets/logo.png';
 
 const NavBar = () => {
+
+    const navLinks = <>
+    <Link className={`navbar-item capitalize text-white hover:text-white hover:border-blue-700 rounded-none pb-4 hover:-pb-3 duration-150 hover:border-b-4 font-medium lg:text-lg`} to={'#skills'}>skills</Link>
+    <Link className={`navbar-item capitalize text-white hover:text-white hover:border-blue-700 rounded-none pb-4 hover:-pb-3 duration-150 hover:border-b-4 font-medium lg:text-lg`} to={'#projects'}>projects</Link>
+    <Link className={`navbar-item capitalize text-white hover:text-white hover:border-blue-700 rounded-none pb-4 hover:-pb-3 duration-150 hover:border-b-4 font-medium lg:text-lg`} to={'#education'}>education</Link>
+    <Link className={`navbar-item capitalize text-white hover:text-white hover:border-blue-700 rounded-none pb-4 hover:-pb-3 duration-150 hover:border-b-4 font-medium lg:text-lg`} to={'#experience'}>experience</Link>
+    <Link className={`navbar-item capitalize text-white hover:text-white hover:border-blue-700 rounded-none pb-4 hover:-pb-3 duration-150 hover:border-b-4 font-medium lg:text-lg`} to={'#aboutme'}>about me</Link>
+</>;
+
+    const [activateSection, setActivateSection] = useState('');
+
+    useEffect(()=>{
+        const handleScroll = ()=>{
+            const sections = document.querySelectorAll('section');
+            let currentSection = '';
+
+            sections.forEach(section=>{
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.clientHeight;
+                if(window.scrollY >= sectionTop-sectionHeight / 3){
+                    currentSection = section.getAttribute('id');
+                }
+            });
+
+            if(currentSection !== activateSection){
+                setActivateSection(currentSection);
+                window.history.pushState(null, null, `#${currentSection}`);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        return ()=>{
+            window.removeEventListener('scroll', handleScroll);
+        }
+    },[]);
     return (
         <div className=''>
             <div className="navbar navbar-sticky m-5 w-[95%] mx-auto rounded-lg backdrop-blur-2xl bg-gray-800">
                 <div className="navbar-start">
                     <a href="#">
-                        <img class="w-auto h-6 sm:h-7" src="https://merakiui.com/images/logo.svg" alt="" />
+                        <img class="w-auto sm:h-7" src={logo} alt="" />
                     </a>
-                    <a className="navbar-item  text-white">MeetUp</a>
+                    {/* <a className="navbar-item  text-white">MeetUp</a> */}
                 </div>
                 <div className="navbar-center">
-                    <NavLink className="navbar-item  text-white">Home</NavLink>
-                    <NavLink className="navbar-item  text-white">Partners</NavLink>
-                    <NavLink className="navbar-item  text-white">Home</NavLink>
-                    <NavLink className="navbar-item  text-white">Home</NavLink>
-                    <NavLink className="navbar-item  text-white">Home</NavLink>
-                    <a className="navbar-item  text-white">About</a>
-                    <a className="navbar-item  text-white">Contact</a>
+                    {
+                        navLinks
+                    }
                 </div>
                 <div class="navbar-end flex items-center mt-2 -mx-2 sm:mt-0 space-x-3">
                     <a href='#' class="btn btn-solid-primary font-semibold">Log In</a>
