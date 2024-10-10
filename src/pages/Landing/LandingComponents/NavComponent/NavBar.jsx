509ca1cca04ad0ecc801dc90ec620e967ee87ee7
GaziMaksudur <gazimaksudur2@gmail.com>
2024-10-10 18:35:43 +0600

    navlink reference added

--- src/pages/Landing/LandingComponents/NavComponent/NavBar.jsx
+++ src/pages/Landing/LandingComponents/NavComponent/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../../../../assets/logo.png';
 
 const NavBar = () => {
