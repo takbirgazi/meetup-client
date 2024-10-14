65ba26da2d036cecdcd3989af2248dd42d468a7b
mr_nobody <mahfuzantor2@gmail.com>
2024-10-14 11:14:04 +0600

    added particlejs to roomjoin page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar bg-black h-[65px]  border-none shadow-none mx-auto rounded-sm">
+    <div className="navbar bg-current h-[65px] shadow-lg shadow-red-300 mx-auto z-50">
       <div className="navbar-start">
         <Link to="/" className="navbar-item font-bold text-white text-xl">
           <img src={logo} className="h-10 w-auto" />
