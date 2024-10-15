be0078f015cdcc9494b2c74451d3060606bb2388
minhaj.dev <minhajhasan.dev@gmail.com>
2024-10-15 15:13:37 +0600

    made responsive landing page sections, toast placement fixed

--- src/pages/Landing/LandingComponents/Testimonials/Testimonials.jsx
+++ src/pages/Landing/LandingComponents/Testimonials/Testimonials.jsx
@@ -200,7 +200,7 @@ const Testimonials = () => {
           <div className="keen-slider__slide mt-3 md:block md:text-left flex flex-col items-center text-center">
             <div className=" mt-2">
               <img
-                className="w-40 h-10 "
+                className="w-40 h-10"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Facebook_logo_%282023%29.svg/1024px-Facebook_logo_%282023%29.svg.png"
                 alt=""
               />
