529bffa9972f79df67ceebf49f323bdcf0e0a8b0
takbirgazi <takbirgazibd@gmail.com>
2024-10-28 12:01:52 +0600

    Update: Work Space

--- src/pages/Dashboard/DashboardComponents/WorkSpace/WorkSpace.jsx
+++ src/pages/Dashboard/DashboardComponents/WorkSpace/WorkSpace.jsx
@@ -2,9 +2,30 @@
 
 const WorkSpace = () => {
     return (
-        <div className="text-white flex flex-col items-center relative min-h-screen p-6">
-            <div>
-                <h2>Work Space</h2>
+        <div className="text-white flex flex-col items-center relative min-h-screen w-full p-6">
+            <div className="w-full flex gap-5 flex-col py-5">
+                {/* Heading  */}
+                <div className="w-72 lg:w-[700px] mx-auto my-8 p-5 rounded-2xl flex items-center backdrop-blur-lg bg-white/10 border border-white/20 shadow-lg">
+                    <h2 className="text-2xl font-semibold text-center w-full">Work Space</h2>
+                </div>
+                {/* Task List and Creation Button  */}
+                <div className="w-11/12 my-8 p-5 rounded-2xl flex flex-col gap-5 items-center backdrop-blur-lg bg-white/10 border border-white/20 shadow-lg">
+                    <h3 className="text-xl font-semibold text-center w-full">Tasks</h3>
+                    {/* Task Card Start*/}
+                    <div className="border rounded-2xl p-3 w-full min-h-56">
+                        Card
+                    </div>
+                    {/* Task Card End*/}
+                </div>
+                {/* Task Progress Section  */}
+                <div className="w-11/12 my-8 p-5 rounded-2xl flex flex-col gap-5 items-center backdrop-blur-lg bg-white/10 border border-white/20 shadow-lg">
+                    <h3 className="text-xl font-semibold text-center w-full">Task Progress</h3>
+                    {/* Task Card Start*/}
+                    <div className="border rounded-2xl p-3 w-full min-h-56">
+                        Card
+                    </div>
+                    {/* Task Card End*/}
+                </div>
             </div>
         </div>
     );
